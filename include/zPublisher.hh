7089e0d1b2cd3d4e4699a44b0113b42371b5ab3d
mirabito <laurent.mirabito@cern.h>
2019-01-17 17:04:11 +0100

    debugged

--- include/zPublisher.hh
+++ include/zPublisher.hh
@@ -11,7 +11,7 @@ namespace zdaq {
   class zPublisher 
   {
   public:
-    zPublisher(std::string hardware,std:;string location,uint32_t tcp,zmq::context_t* c) : _hardware(hardware),_location(location),_tcpPort(tcp),_context(c)
+    zPublisher(std::string hardware,std::string location,uint32_t tcp,zmq::context_t* c) : _hardware(hardware),_location(location),_tcpPort(tcp),_context(c)
     {
         _publisher= new zmq::socket_t((*_context), ZMQ_PUB);
 	  std::stringstream sport;
@@ -38,7 +38,7 @@ namespace zdaq {
 	  zmq::message_t ma1((void*)head.c_str(), head.length(), NULL); 
 	  _publisher->send(ma1, ZMQ_SNDMORE);
 	  // Status
-	  Json::Value jstatus=this->status();
+	  Json::Value jstatus=status;
 	  std::string scont= fastWriter.write(jstatus);
 	  zmq::message_t ma2((void*)scont.c_str(), scont.length(), NULL); 
 
@@ -46,9 +46,10 @@ namespace zdaq {
 	  _publisher->send(ma2);
 	 
     }
-    
+    std::string hardware(){return _hardware;}
+		std::string location() {return _location;}
   protected:
-    std::string _hardware,_location
+    std::string _hardware,_location;
     uint32_t _tcpPort;
     zmq::context_t* _context;
     zmq::socket_t *_publisher;
