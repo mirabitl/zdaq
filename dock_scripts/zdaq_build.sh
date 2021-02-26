e325d3420cb2b896e3223dd6941504b957e098b8
mirabitl <laurent.mirabito@cern.ch>
2021-02-26 13:18:47 +0100

    Script to create zdaq docker images

---
+++ dock_scripts/zdaq_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker build -t zdaq:1 -f- https://github.com/mirabitl/zdaq.git <<EOF
+FROM debian:buster
+RUN set -eux; \
+    apt update; \
+    apt -y install wget;\
+    wget https://raw.githubusercontent.com/mirabitl/zdaq/master/docker_zdaq_install.sh;\
+    chmod +x docker_zdaq_install.sh;\
+    ./docker_zdaq_install.sh;
+EOF
